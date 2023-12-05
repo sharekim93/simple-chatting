a08dadd7066529e88f40dfd9ae1a275febb953fb
sharekim <sksyag@naver.com>
2023-12-05 10:39:40 +0900

    feat: Topbar 구현

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,7 +1,21 @@
+import { User } from "firebase/auth";
+import Image from "next/image";
 import React from "react";
 
-const TopBar = () => {
-  return <div>TopBar</div>;
+const TopBar = ({ user }: { user: User }) => {
+  return (
+    <div className="h-[70px] border-b-[1px] space-x-4 flex items-center px-6 py-4">
+      <Image
+        src={user?.photoURL!}
+        alt={user?.displayName!}
+        width={40}
+        height={40}
+        className="object-cover rounded-full"
+      />
+
+      <span className="font-bold">{user?.displayName}</span>
+    </div>
+  );
 };
 
 export default TopBar;
